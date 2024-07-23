71c075f08a2373dbb25a7b01dc69c8cf19c6e9b9
swJo <kjsw12@naver.com>
2024-07-23 16:55:59 +0900

    feat: api 코드 작성

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -23,14 +23,16 @@ export interface SearchBoardResponse {
   content: Board[];
   totalPages: number;
   totalElements: number;
+  last: boolean;
 }
 
-const API_URL = process.env.NEXT_PUBLIC_APP_URL;
+const API_URL = process.env.NEXT_PUBLIC_LOCAL_BACKED_URL;
 
 export const searchBoard = async (
   params: SearchBoardParams
 ): Promise<SearchBoardResponse> => {
   const url = `board${makeQuerystring(params)}`;
+  console.log(API_URL)
   const response = await axios.get(`${API_URL}/${url}`);
   console.log(response.data.content)
   console.log(response.data)
@@ -38,16 +40,30 @@ export const searchBoard = async (
 };
 
 export const getBoard = async (id: number): Promise<Board> => {
-  const response = await axios.get(`${API_URL}/${id}`);
+  const response = await axios.get(`${API_URL}/board/${id}`);
   return response.data;
 };
 
-export const updateBoard = async (id: number, post: Board): Promise<Board> => {
-  const response = await axios.put(`${API_URL}/${id}`, post);
+export const updateBoard = async (id: number, post: any): Promise<any> => {
+  const response = await axios.patch(`${API_URL}/board/${id}`, post);
   return response.data;
 };
 
-export const deleteBoard = async (id: number, password: string): Promise<Board> => {
-  const response = await axios.post(`${API_URL}/${id}`, password);
+export const deleteBoard = async (id: number, password: any): Promise<any> => {
+  const response = await axios.post(`${API_URL}/board/${id}`, password);
   return response.data;
 };
+
+export const getCommentList = async (id: number): Promise<Board> => {
+  const response = await axios.get(`${API_URL}/board/${id}/comment`);
+  return response.data;
+};
+
+export const createComment = async (id: number, comment: any): Promise<Board> => {
+  const response = await axios.post(`${API_URL}/board/${id}/comment`, comment);
+  return response.data;
+};
+
+
+
+
